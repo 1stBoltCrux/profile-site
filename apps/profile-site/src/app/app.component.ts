efa7641d93bb37509c6921f6e5b1001d4eccda1e
Ted Leary <ted.e.leary@ey.com>
2020-08-05 08:35:55 -0700

    initial commit

---
+++ apps/profile-site/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'profile-site-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'profile-site';
+}
